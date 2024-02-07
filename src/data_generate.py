035aa77b3b92fa1a318c571726d6c345400c7b2c
HoyeonS <100548830+HoyeonS@users.noreply.github.com>
2024-02-06 18:01:13 -0600

    include dataset and constant for data processing

---
+++ src/data_generate.py
@@ -0,0 +1,25 @@
+import os
+import constants
+import numpy as np
+
+# !! DATA path to store the folder to detect
+DATA_PATH = os.path.join('dataset')
+
+actions = np.array(constants.ACTION_LIST)
+
+np_sequence = constants.NP_SEQUENCE
+
+np_constant = constants.NP_LENGTH
+
+def main():
+
+    for action in actions:
+        for sequence in range(np_sequence):
+            try:
+                os.makedirs(os.path.join(DATA_PATH, action, str(sequence)))
+            except:
+                print("DATA GENERATION FAILED")
+                pass
+
+if __name__ == "__main__":
+    main()
--- src/mcapture.py
+++ src/mcapture.py
@@ -1,10 +1,10 @@
 import cv2
-import numpy
+import numpy as np
 import os 
 from matplotlib import pyplot
 import time
 import mediapipe
-
+import constants
 #holistic model for image detection taking landmarks
 mp_holistic = mediapipe.solutions.holistic
 
@@ -24,13 +24,25 @@ def draw_landmarks(img, results):
     mp_drawing.draw_landmarks(img, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
     mp_drawing.draw_landmarks(img, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
     mp_drawing.draw_landmarks(img, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+
+def extract_key_values(results):
+    pose = []
+    for res in results.pose_landmarks.landmark:
+        test = np.array([res.x, res.y, res.z, res.visibility])
+        pose.append(test)
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(constants.SIZE_POSE_LANDMARK)
+    face = np.array([[res.x, res.y, res.z, res.visibility] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(constants.SIZE_FACE_LANDMARK)
+    left_hand = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(constants.SIZE_LEFT_HAND_LANDMARK)
+    right_hand = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(constants.SIZE_RIGHT_HAND_LANDMARK)
+    return np.concatenate([pose, face, left_hand, right_hand])
 def main():
     # VideoCapture with input 0 will call the camera to get motion captured
     # Reference: https://docs.opencv.org/3.4/d8/dfe/classcv_1_1VideoCapture.html#ae82ac8efcff2c5c96be47c060754a518
     cap = cv2.VideoCapture(0)
 
     # Returns true if video capturing has been initialized already
-    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    with mp_holistic.Holistic(min_detection_confidence=constants.MIN_DETECTION_CONFIDENCE,
+                               min_tracking_confidence=constants.MIN_TRACKING_CONFIDENCE) as holistic:
         while cap.isOpened():
             # Grabs, decodes and returns the next video frame
             # ret :
@@ -38,12 +50,12 @@ def main():
             ret, frame = cap.read()
             img, results = mediapipe_detection(frame,holistic)
             draw_landmarks(img, results)
-            cv2.imshow('Motion Capture', img)
+            cv2.imshow(constants.NAME_FRAME, img)
             print(results)
             #break for frame closure with typing 'q'
-            if cv2.waitKey(10) & 0xFF == ord('q'):
+            if cv2.waitKey(10) & 0xFF == ord(constants.KILL_PROCESS_KEY_INPUT):
                 break
-    
+
     cap.release()
     cv2.destroyAllWindows()
 
