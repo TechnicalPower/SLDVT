38780c2e1ae19dbee97e2307637a27c8c318a431
Juyeong <148512003+jyjy990812@users.noreply.github.com>
2024-03-18 17:37:05 -0500

    Update run.py

--- src/run.py
+++ src/run.py
@@ -1,11 +1,7 @@
 import voice_translate
 import ui
-## Instead of change docker file for application running, change this file.
-
 
 ## starts with running default 
-var_run = input("PICK WORKFLOW || voice : '1' || ui(main) : '2'")
-if(var_run == "1"):
-    voice_translate.main()
-elif(var_run == "2"):
+
+if __name__ == "__main__":
     ui.main()
