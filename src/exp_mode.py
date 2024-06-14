5e422d98ec3a11bf89741e34a3b8c13b39583467
HoyeonS <100548830+HoyeonS@users.noreply.github.com>
2024-06-13 21:44:06 -0700

    Create exp_mode.py

---
+++ src/exp_mode.py
@@ -0,0 +1,13 @@
+import sys
+ from workflows.basic_workflow.learn import Learn
+ from utils.data_generate import custom_flow_data_generate
+
+
+ def main():
+     word = input("Currently in experimental mode... Could you tell me which word you want to teach for me?")
+     custom_flow_data_generate(word)
+     basic_learn = Learn()
+     basic_learn.start_capture(word)
+
+ if __name__ == "__main__":
+     main()
